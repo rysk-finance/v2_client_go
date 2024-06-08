3873ddefb9fa54797f00fec3984b4063f072ca48
eldief <47423029+eldief@users.noreply.github.com>
2024-06-08 18:35:53 +0200

    add ws stream endpoint

--- constants/ws_url.go
+++ constants/ws_url.go
@@ -3,6 +3,11 @@ package constants
 import "github.com/eldief/go100x/types"
 
 var WS_URL = map[types.Environment]string{
-	ENVIRONMENT_MAINNET: "",
+	ENVIRONMENT_MAINNET: "wss://api.100x.finance/v1/ws/operate",
 	ENVIRONMENT_TESTNET: "wss://api.staging.100x.finance/v1/ws/operate",
+}
+
+var WS_STREAM_URL = map[types.Environment]string{
+	ENVIRONMENT_MAINNET: "wss://stream.100x.finance/",
+	ENVIRONMENT_TESTNET: "wss://stream.staging.100x.finance/",
 }
